c96501cc6a7a8067591a2542198fafac9aa4e687
Vladislav Semchenok <totalkhl@mail.ru>
2023-08-17 00:37:51 +0500

    if

--- test_app/src/Main.java
+++ test_app/src/Main.java
@@ -1,7 +1,27 @@
-import lesson_1.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Robot.start();
-        System.out.println(Rollback.apple());
+      enter_operation();
+    }
+    public static void enter_operation() {
+
+        Scanner obj_input = new Scanner(System.in);
+
+        System.out.println("Введите число от 1 до 3");
+
+        int number = obj_input.nextInt();
+
+        if (number == 1) {
+            System.out.println("Вы ввели число 1");
+        }
+        else if (number == 2) {
+            System.out.println("Вы ввели число 2");
+        }
+        else if (number == 3) {
+            System.out.println("Вы ввели число 3");
+        }
+        else {
+            System.out.println("Вы ввели число больше 3");
+        }
     }
 }
