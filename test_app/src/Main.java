4d64e785ec82468f53c7e34f8a1afa1d98160baa
Vladislav Semchenok <totalkhl@mail.ru>
2023-08-17 14:02:44 +0500

    kotlin_app

--- test_app/src/Main.java
+++ test_app/src/Main.java
@@ -1,27 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-      enter_operation();
-    }
-    public static void enter_operation() {
-
-        Scanner obj_input = new Scanner(System.in);
-
-        System.out.println("Введите число от 1 до 3");
-
-        int number = obj_input.nextInt();
-
-        if (number == 1) {
-            System.out.println("Вы ввели число 1");
-        }
-        else if (number == 2) {
-            System.out.println("Вы ввели число 2");
-        }
-        else if (number == 3) {
-            System.out.println("Вы ввели число 3");
-        }
-        else {
-            System.out.println("Вы ввели число больше 3");
-        }
+        Person tom = new Person();
+        tom.displayInfo();
     }
 }
+
+class Person{
+    String name;
+    int age;
+    void displayInfo(){
+        System.out.println("Name: %s \tAge: %d\n");
+    }
+}
+
+
