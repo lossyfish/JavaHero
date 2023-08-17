5cc41e998f3ca6cd4af3b50914ae9ba869f96e8e
Vladislav Semchenok <totalkhl@mail.ru>
2023-08-17 17:30:01 +0500

    kotlin_push

--- app_kotlin/src/main/kotlin/Main.kt
+++ app_kotlin/src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+/*Тестовый многострочный комментарий*/
+fun main() {
+
+    print("Введите имя: ")
+    val name = readLine()
+    print("Введите email: ")
+    val email = readLine()
+    print("Введите адрес: ")
+    val address = readLine()
+
+    println("Ваше имя: $name")
+    println("Ваш email: $email")
+    println("Ваш адрес: $address")
 }
